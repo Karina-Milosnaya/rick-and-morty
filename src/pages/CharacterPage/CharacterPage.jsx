6470aaf4ea92061c273b03bdc42d496d41e0cb91
Karina-Milosnaya <k.milosnaya@gmail.com>
2025-08-06 16:04:35 +0400

    add useState and useEffect implementation to CharacterPage to get data from API to display characters on the page

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -1,7 +1,37 @@
-// import s from './CharacterPage.module.css'
+import s from './CharacterPage.module.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export const CharacterPage = () => {
+
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/character').then((response) => {
+            setCharacters(response.data.results);
+        })
+    }, []);
+
+
     return(
-        <h1 className={'pageTitle'}>CharacterPage</h1>
+        <div className={'pageContainer'}>
+            <h1 className={'pageTitle'}>CharacterPage</h1>
+            {characters.length && (
+                <div className={s.characters}>
+                    {
+                        characters.map(character => {
+                            return (
+                                <div className={s.character} key={character.id}>
+                                    <div className={s.characterLink}>{character.name}</div>
+                                    <img src={character.image} alt={`${character.name} avatar`} />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            )}
+
+        </div>
+
     )
 }
